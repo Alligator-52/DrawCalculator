321d80302e8f08fe26bf22ec6d45296ea8275be8
Alligator-52 <ayushankitpatnaik99@gmail.com>
2024-07-25 00:29:26 +0530

    Update ShuntingYard.java

--- app/src/main/java/com/alligator/drawcalculator/ShuntingYard.java
+++ app/src/main/java/com/alligator/drawcalculator/ShuntingYard.java
@@ -18,7 +18,34 @@ public class ShuntingYard
             if(Character.isDigit(token) || token == '.')
             {
                 StringBuilder number = new StringBuilder();
-                
+                while(i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.'))
+                {
+                    number.append(expression.charAt(i++));
+                }
+                i--;
+                output.add(number.toString());
+            }
+            else if (token == '(')
+            {
+                operators.push(String.valueOf(token));
+            }
+            else if (token == ')')
+            {
+                while(!operators.isEmpty() && !operators.peek().equals("("))
+                {
+                    output.add(operators.pop());
+                }
+            }
+            else
+            {
+                StringBuilder operator = new StringBuilder();
+                while(i < expression.length() && Character.isLetter(expression.charAt(i)))
+                {
+                    operator.append(expression.charAt(i++));
+                }
+                i--;
+
+                String op = operator.toString();
             }
         }
     }
